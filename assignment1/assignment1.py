1d359f6308aed7f1904b5bda03d931c35ca2e831
Tom den Braber <tomdenbraber@gmail.com>
2015-11-18 13:15:52 +0100

    fixed some file names, cleaned up code

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -11,36 +11,26 @@ git = sh.git.bake(_cwd='lucene-solr')
 ### read a json file
 
 # load all files
-maxIssueNr = -1
-
-closed = 0
-
 decoder = JSONDecoder()
 PATH = "issue_LUCENE"
 TIMEFORMAT = "%Y-%m-%dT%H:%M:%S.000+0000"
 START = mktime(strptime("2015-01-01T00:00:00.000+0000", TIMEFORMAT))
 END = mktime(strptime("2015-07-01T00:00:00.000+0000", TIMEFORMAT))
 
-
 issues = []
-
-jsonF = open(PATH + "/12314162.json")
-decodedJson = decoder.decode(jsonF.read())
-
-#print(decodedJson['fields']['resolutiondate'])#.keys())
 
 def isClosedResolved(issue):
     return issue['fields']['status']['name'] == "Closed" and issue['fields']['resolution']['name'] == "Fixed"
 
 def isCorrectTimePeriod(issue):
-    t = mktime(strptime(decodedJson['fields']['resolutiondate'], TIMEFORMAT))
+    t = mktime(strptime(issue['fields']['resolutiondate'], TIMEFORMAT))
     return t >= START and t < END
 
 for f in listdir(PATH):
     jsonF = open(PATH + "/" + f)
-    decodedJson = decoder.decode(jsonF.read())
-    if isClosedResolved(decodedJson) and isCorrectTimePeriod(decodedJson):
-        issues.append(decodedJson['key'])
+    issue = decoder.decode(jsonF.read())
+    if isClosedResolved(issue) and isCorrectTimePeriod(issue):
+        issues.append(issue['key'])
 
 print(issues)
 print(len(issues))
