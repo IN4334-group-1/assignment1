7d976dc2a6457bb9588d804da77981e91e607f74
Tom den Braber <tomdenbraber@gmail.com>
2016-01-06 13:30:13 +0100

    fixed datasets, added script for that purpose

---
+++ final-assignment/scripts/augment-dataset.py
@@ -0,0 +1,34 @@
+from csv import DictReader, DictWriter
+
+
+fileList = ["avg-followers-per-project", "max-followers-per-project", "forks-per-project", 
+    "nr-of-pull-requests-per-project", "number-of-commits-per-project", "stars-per-project"]
+
+
+for fileName in fileList:
+    with open('exports/!' + fileName + '.csv') as csvfile:
+        reader = DictReader(csvfile)
+        
+        f = open("samples/comma-samples-combined.csv")
+        finalDict = []
+        badIds = f.read().split(", ")
+        ids = []
+        for i in badIds:
+            ids.append(i.replace('"', ""))
+        
+        for row in reader:
+            currentId = row[reader.fieldnames[0]]
+            currentVal = row[reader.fieldnames[1]]
+            finalDict.append({reader.fieldnames[0]: currentId, reader.fieldnames[1]: currentVal})
+            ids.remove(currentId)
+        
+        for i in ids:
+            finalDict.append({reader.fieldnames[0]: i, reader.fieldnames[1]: 0})
+        
+        with open('exports/' + fileName + '.csv', 'w') as csvfile:
+            fieldnames = reader.fieldnames
+            writer = DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for finalD in finalDict:
+                writer.writerow(finalD)
